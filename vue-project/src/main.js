685620374e899386289efd6373da05409c2bef77
Kathryn Oroceo <122486076+kporoceo@users.noreply.github.com>
2025-01-23 18:41:17 +0800

    import bootstrap

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap'; // Import Bootstrap JavaScript
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap'; 
 
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
