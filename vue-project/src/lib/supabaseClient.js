6691b6812801faf0f1df0e6a371c51f00d5a17a1
Kathryn Oroceo <122486076+kporoceo@users.noreply.github.com>
2025-01-27 17:50:13 +0800

    supabase step 4, cont. in codespace

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://utvnjubgjvtuelfssekj.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dm5qdWJnanZ0dWVsZnNzZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgzMjUsImV4cCI6MjA1MzU0NDMyNX0.MHckHOu1S3quJmVWxFarqANvI7qK8mDo-vClDKvjEAE>')
