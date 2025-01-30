cf65024646eb9baa36cd08e1c1e460fbdf06f9f2
Kathryn Oroceo <122486076+kporoceo@users.noreply.github.com>
2025-01-30 08:37:17 +0000

    <> removed

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://utvnjubgjvtuelfssekj.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dm5qdWJnanZ0dWVsZnNzZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgzMjUsImV4cCI6MjA1MzU0NDMyNX0.MHckHOu1S3quJmVWxFarqANvI7qK8mDo-vClDKvjEAE';
 
-export const supabase = createClient('https://utvnjubgjvtuelfssekj.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dm5qdWJnanZ0dWVsZnNzZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgzMjUsImV4cCI6MjA1MzU0NDMyNX0.MHckHOu1S3quJmVWxFarqANvI7qK8mDo-vClDKvjEAE>')
+export const supabase = createClient('https://utvnjubgjvtuelfssekj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dm5qdWJnanZ0dWVsZnNzZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgzMjUsImV4cCI6MjA1MzU0NDMyNX0.MHckHOu1S3quJmVWxFarqANvI7qK8mDo-vClDKvjEAE')
